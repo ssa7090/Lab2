9accaf26e42ef67d201cddf4b5ff124007561101
ssa7090 <ssa7090@students.fhv.at>
2022-04-29 13:05:58 +0200

    setup Environment

--- src/main/java/at/fhv/sysarch/lab2/homeautomation/HomeAutomationController.java
+++ src/main/java/at/fhv/sysarch/lab2/homeautomation/HomeAutomationController.java
@@ -10,9 +10,12 @@ import akka.actor.typed.javadsl.Behaviors;
 import akka.actor.typed.javadsl.Receive;
 import at.fhv.sysarch.lab2.homeautomation.devices.AirCondition;
 import at.fhv.sysarch.lab2.homeautomation.devices.TemperatureSensor;
+import at.fhv.sysarch.lab2.homeautomation.environment.Environment;
 import at.fhv.sysarch.lab2.homeautomation.ui.UI;
 
 public class HomeAutomationController extends AbstractBehavior<Void>{
+
+    private ActorRef<Environment.EnvironmentCommand> environment;
     private ActorRef<TemperatureSensor.TemperatureCommand> tempSensor;
     private  ActorRef<AirCondition.AirConditionCommand> airCondition;
 
@@ -23,6 +26,7 @@ public class HomeAutomationController extends AbstractBehavior<Void>{
     private  HomeAutomationController(ActorContext<Void> context) {
         super(context);
         // TODO: consider guardians and hierarchies. Who should create and communicate with which Actors?
+        this.environment = getContext().spawn(Environment.create(this.environment))
         this.airCondition = getContext().spawn(AirCondition.create("2", "1"), "AirCondition");
         this.tempSensor = getContext().spawn(TemperatureSensor.create(this.airCondition, "1", "1"), "temperatureSensor");
         ActorRef<Void> ui = getContext().spawn(UI.create(this.tempSensor, this.airCondition), "UI");
---
+++ src/main/java/at/fhv/sysarch/lab2/homeautomation/environment/Environment.java
@@ -0,0 +1,69 @@
+package at.fhv.sysarch.lab2.homeautomation.environment;
+
+import akka.actor.typed.Behavior;
+import akka.actor.typed.javadsl.*;
+
+import java.time.Duration;
+import java.util.Optional;
+
+
+public class Environment extends AbstractBehavior<Environment.EnvironmentCommand> {
+
+    public interface EnvironmentCommand {}
+
+    public static final class TemperatureChange implements EnvironmentCommand {
+        final Optional<Double> value;
+
+        public TemperatureChange (Optional<Double> value) {
+            this.value = value;
+        }
+    }
+
+    public static final class WeatherChange implements EnvironmentCommand {
+        final Optional<Weather> value;
+
+        public WeatherChange (Optional<Weather> value) {
+            this.value = value;
+        }
+    }
+
+    private TimerScheduler<EnvironmentCommand> temperatureTimeScheduler;
+    private TimerScheduler<EnvironmentCommand> weatherTimeScheduler;
+
+    private double temperature = 20.0;
+    private Weather weather = Weather.SUNNY;
+
+    public static Behavior<EnvironmentCommand> create() {
+        return Behaviors.setup(context -> Behaviors.withTimers(timers -> new Environment(context, timers, timers)));
+    }
+
+    private Environment(ActorContext<EnvironmentCommand> context, TimerScheduler<EnvironmentCommand> temperatureTimeScheduler, TimerScheduler<EnvironmentCommand> weatherTimeScheduler) {
+        super(context);
+        this.temperatureTimeScheduler = temperatureTimeScheduler;
+        this.weatherTimeScheduler = weatherTimeScheduler;
+        this.temperatureTimeScheduler.startTimerAtFixedRate(new TemperatureChange(Optional.of(temperature)), Duration.ofSeconds(5));
+        this.weatherTimeScheduler.startTimerAtFixedRate(new WeatherChange(Optional.of(Weather.SUNNY)), Duration.ofSeconds(20));
+
+        getContext().getLog().info("Environment initialized");
+    }
+
+    @Override
+    public Receive<EnvironmentCommand> createReceive() {
+        return newReceiveBuilder()
+                .onMessage(TemperatureChange.class, this::onTemperatureChange)
+                .onMessage(WeatherChange.class, this::onWeatherChange)
+                .build();
+    }
+
+    private Behavior<EnvironmentCommand> onTemperatureChange (TemperatureChange tc) {
+        getContext().getLog().info("TemperatureChange received {}", tc.value.get());
+        this.temperature = tc.value.get();
+        return this;
+    }
+
+    private Behavior<EnvironmentCommand> onWeatherChange (WeatherChange wc) {
+        getContext().getLog().info("WeatherChange received {}", wc.value.get());
+        this.weather = wc.value.get();
+        return this;
+    }
+}
